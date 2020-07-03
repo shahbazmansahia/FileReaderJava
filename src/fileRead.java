1c16c01618db8c081bb0b4aa5538c10dcb35f392
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2020-07-03 00:07:56 -0700

    picks up file, checks if its empty, process words and adds them reversed to array

--- src/fileRead.java
+++ src/fileRead.java
@@ -8,14 +8,15 @@ public class fileRead {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		String fName = "";
-		
+		Vector <String> revWords = new Vector <String> ();		// Vector for storing reversed words form file
 		System.out.println("Please input file name: \n");
 		Scanner scan = new Scanner (System.in);
 		fName = scan.nextLine();
+		String temp = "";
 		
 		File fAccess = new File (fName);
 		
-		if (fAccess.exists()) {
+		if (fAccess.exists()) {				// checks if file exists and can be accessed
 			System.out.println("File Accessed!");
 		}
 		else {
@@ -27,18 +28,33 @@ public class fileRead {
 		
 		//if (fAccess) 					// FIX ME: CHECK IF FILE IS EMPTY OR EVEN A TXT FILE!
 		
+		if (fAccess.length() == 0) {	// Checks if file is empty or not
+			System.out.println("The file is empty!\nexiting...");
+			return;
+		}
+		
 		try (FileReader fRead = new FileReader (fName); // FIX ME: PARSE TXT FILE FOR DESIRED INPUT AND ESTABLISH CHECKS!
 			 Scanner scanFile = new Scanner (fRead);){
-			line = scanFile.next();
-			while (line != null) {
-				
+			//line = scanFile.next();
+			while (scanFile.hasNext()) {
+				line = scanFile.next();
+				for (int i = line.length() - 1; ((i >= 0) && (line.length() > 3)); i--) {
+					temp += line.charAt(i);
+				}
+				if (temp.length() > 3) {
+					revWords.add(temp);
+				}
+				System.out.println(temp + "\n");
+				temp = "";
 			}
 			
+			scanFile.close();
 		}
 		catch(IOException e){
 			System.out.println ("Error! " + e);
 		}
 		
+		scan.close();
 		
 		
 	}
